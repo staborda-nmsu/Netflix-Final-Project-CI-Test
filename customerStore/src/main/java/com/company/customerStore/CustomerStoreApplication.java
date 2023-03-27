58d98938961a3f830fca28e017714320a53cb496
Ahmni <ahmni@ku.edu>
2023-03-27 16:49:22 -0500

    init

---
+++ customerStore/src/main/java/com/company/customerStore/CustomerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.company.customerStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerStoreApplication.class, args);
+	}
+
+}
---
+++ customerStore/src/test/java/com/company/customerStore/CustomerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.customerStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
