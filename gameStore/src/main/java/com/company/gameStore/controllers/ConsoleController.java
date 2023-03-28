d4da2921353704aba5cde14d9263a6f8ff162164
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-03-27 18:31:46 -0600

    add console model, repo, and tests

--- gameStore/src/main/java/com/company/gameStore/controllers/ConsoleController.java
+++ gameStore/src/main/java/com/company/gameStore/controllers/ConsoleController.java
@@ -1,2 +1,59 @@
-package com.company.gameStore.controllers;public class ConsoleController {
+package com.company.gameStore.controllers;
+
+import com.company.gameStore.models.Console;
+import com.company.gameStore.repositories.ConsoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+
+@RestController
+public class ConsoleController {
+    private final ConsoleRepository consoleRepository;
+
+    @Autowired
+    public ConsoleController(ConsoleRepository consoleRepository) {
+        this.consoleRepository = consoleRepository;
+    }
+
+    @GetMapping("/consoles")
+    public List<Console> getAllConsoles() {
+        return consoleRepository.findAll();
+    }
+
+    @GetMapping("/consoles/{consoleId}")
+    public Console getConsoleById(@PathVariable int consoleId) {
+        return consoleRepository.findById(consoleId).orElse(null);
+    }
+
+    @PostMapping("/consoles")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Console addConsole(@RequestBody Console console) {
+        return consoleRepository.save(console);
+    }
+
+    @PutMapping("/consoles")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console) {
+        consoleRepository.save(console);
+    }
+
+    @DeleteMapping("/consoles/{consoleId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable int consoleId) {
+        try {
+            consoleRepository.deleteById(consoleId);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Console not found", ex);
+        }
+    }
+
+    @GetMapping("/consoles/manufacturer/{manufacturer}")
+    public List<Console> getConsoleByManufacturer(@PathVariable String manufacturer) {
+        return consoleRepository.findByManufacturer(manufacturer);
+    }
 }
--- gameStore/src/main/java/com/company/gameStore/repositories/ConsoleRepository.java
+++ gameStore/src/main/java/com/company/gameStore/repositories/ConsoleRepository.java
@@ -1,2 +1,10 @@
-package com.company.gameStore.repositories;public class ConsoleRepository {
+package com.company.gameStore.repositories;
+
+import com.company.gameStore.models.Console;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ConsoleRepository extends JpaRepository<Console, Integer> {
+    List<Console> findByManufacturer(String manufacturer);
 }
