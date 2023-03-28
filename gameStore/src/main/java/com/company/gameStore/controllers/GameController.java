8767e753a5df63d142613b3eb005d996947a4eae
Ahmni <ahmni@ku.edu>
2023-03-27 19:35:01 -0500

    merge

--- gameStore/src/main/java/com/company/gameStore/controllers/GameController.java
+++ gameStore/src/main/java/com/company/gameStore/controllers/GameController.java
@@ -4,6 +4,7 @@ import com.company.gameStore.models.Game;
 import com.company.gameStore.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,57 +12,71 @@ import java.util.Optional;
 
 @RestController
 public class GameController {
-    @Autowired
-    private GameRepository repo;
+        @Autowired
+        private GameRepository repo;
 
-    @GetMapping("/games")
-    public List<Game> getGames() {
-        return repo.findAll();
-    }
+        @GetMapping("/games")
+        public List<Game> getGames() {
+            return repo.findAll();
+        }
 
-    @GetMapping("/games/studio/{studio}")
-    public Game getGameByStudio(@PathVariable String studio) {
-        Optional<Game> returnVal = repo.findByStudio(studio);
-        if (returnVal.isPresent()) {
-            return returnVal.get();
-        } else {
-            return null;
+        @GetMapping("/games/studio/{studio}")
+        public ResponseEntity<Game> getGameByStudio(@PathVariable String studio) {
+            Optional<Game> returnVal = repo.findByStudio(studio);
+            if (returnVal.isPresent()) {
+                return ResponseEntity.ok(returnVal.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @GetMapping("/games/esrb/{esrb}")
+        public ResponseEntity<Game> getGameByESRB(@PathVariable String esrb) {
+            Optional<Game> returnVal = repo.findByEsrbRating(esrb);
+            if (returnVal.isPresent()) {
+                return ResponseEntity.ok(returnVal.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @GetMapping("/games/title/{title}")
+        public ResponseEntity<Game> getGameByTitle(@PathVariable String title) {
+            Optional<Game> returnVal = repo.findByTitle(title);
+            if (returnVal.isPresent()) {
+                return ResponseEntity.ok(returnVal.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @PostMapping("/games")
+        public ResponseEntity<Game> addGame(@RequestBody Game game) {
+            Game savedGame = repo.save(game);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedGame);
+        }
+
+        @PutMapping("/games/{id}")
+        public ResponseEntity<Void> updateGame(@PathVariable int id, @RequestBody Game game) {
+            Optional<Game> existingGame = repo.findById(id);
+            if (existingGame.isPresent()) {
+                game.setId(id);
+                repo.save(game);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @DeleteMapping("/games/{id}")
+        public ResponseEntity<Void> deleteGame(@PathVariable int id) {
+            Optional<Game> existingGame = repo.findById(id);
+            if (existingGame.isPresent()) {
+                repo.deleteById(id);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
         }
     }
-    @GetMapping("/games/esrb/{esrb}")
-    public Game getGameByESRB(@PathVariable String esrb) {
-        Optional<Game> returnVal = repo.findByEsrbRating(esrb);
-        if (returnVal.isPresent()) {
-            return returnVal.get();
-        } else {
-            return null;
-        }
-    }
-    @GetMapping("/games/title/{title}")
-    public Game getGameByTitle(@PathVariable String title) {
-        Optional<Game> returnVal = repo.findByTitle(title);
-        if (returnVal.isPresent()) {
-            return returnVal.get();
-        } else {
-            return null;
-        }
-    }
 
-    @PostMapping("/games")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Game addPublisher(@RequestBody Game Publisher) {
-        return repo.save(Publisher);
-    }
-
-    @PutMapping("/games")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updatePublisher(@RequestBody Game Publisher) {
-        repo.save(Publisher);
-    }
-
-    @DeleteMapping("/games/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePublisher(@PathVariable int id) {
-        repo.deleteById(id);
-    }
-}
