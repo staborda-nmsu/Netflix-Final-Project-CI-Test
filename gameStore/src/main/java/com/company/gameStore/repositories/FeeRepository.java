22f104d583a7676d23e6fb12a068778fd9006667
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2023-03-28 00:23:04 -0500

    added fee repository

---
+++ gameStore/src/main/java/com/company/gameStore/repositories/FeeRepository.java
@@ -0,0 +1,9 @@
+package com.company.gameStore.repositories;
+
+import com.company.gameStore.models.Fee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeeRepository extends JpaRepository<Fee, Integer> {
+}
